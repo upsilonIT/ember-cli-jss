88d792062a030d8d72ec4aa7beb85b2b708220b0
Timofey Dergachev <t@exeto.me>
2018-04-04 23:22:34 +0300

    Add tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,14 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import jss from 'jss';
+import preset from 'jss-preset-default';
 
 setApplication(Application.create(config.APP));
 
 start();
+
+jss.setup({
+  plugins: preset().plugins,
+  createGenerateClassName: () => rule => `jss--${rule.key}`,
+});
