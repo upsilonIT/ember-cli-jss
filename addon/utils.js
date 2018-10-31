e6cd6f75520023f90d0584d3740ab99cdf8ac259
Timofey Dergachev <t@exeto.me>
2018-10-31 11:17:48 +0300

    Fix getting id

--- addon/utils.js
+++ addon/utils.js
@@ -8,3 +8,7 @@ export const uniqKey = name => `__${name}__${random()}`;
 export const isEmpty = (obj = {}) => !Object.keys(obj).length;
 
 export const isBool = value => typeof value === 'boolean';
+
+export const getId = ctx => String(ctx).match(/:(\w+)>$/)[1];
+
+export const getComponentName = ctx => String(ctx).match(/:(.+?):/)[1];
