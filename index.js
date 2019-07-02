d8ff0b90bd258b9f85bab773870f2e6ce2e40898
Timofey Dergachev <t@exeto.me>
2019-07-02 11:42:48 +0300

    Update jss and jss-preset-default to version 10.0.0-alpha.21

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const HtmlbarsPlugin = require('./lib/htmlbars-plugin');
 module.exports = {
   name: 'ember-cli-jss',
 
-  included(app) {
-    app.import('node_modules/jss/dist/jss.min.js', {
-      using: [{ transformation: 'amd', as: 'jss' }],
-    });
-
-    app.import(
-      'node_modules/jss-preset-default/dist/jss-preset-default.min.js',
-      {
-        using: [{ transformation: 'amd', as: 'jss-preset-default' }],
-      },
-    );
-  },
-
   setupPreprocessorRegistry(type, registry) {
     if (type !== 'parent') {
       return;
